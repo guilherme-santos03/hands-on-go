ca53bfaeba7047f13e140c2776cf4c0fae807a55
Guilherme Santos <guilherme.santos@kantar.com>
2024-11-06 14:37:22 +0100

    solve 3rd party pkg challenge

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -2,9 +2,15 @@
 package main
 
 // import the proverbs package
+import "github.com/jboursiquot/go-proverbs"
+import "fmt"
 
 // getRandomProverb returns a random proverb from the proverbs package
+func getRandomProverb() string {
+	return proverbs.Random().Saying
+}
 
 func main() {
+	fmt.Println(getRandomProverb())
 	// print the result of calling your getRandomProverb function
 }
